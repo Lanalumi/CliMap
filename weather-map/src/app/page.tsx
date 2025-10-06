8f3ecd4144ce09a01931d80d620c3af97b621d16
Lanalumi <lanalumi97@gmail.com>
2025-10-05 23:59:58 -0300

    adds map

--- weather-map/src/app/page.tsx
+++ weather-map/src/app/page.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import MapImg from "./assets/Map.jpeg";
 
 export default function Home() {
   return (
     <div>
-      <main></main>
+      <main>
+        <div className="map-container">
+          <Image src={MapImg} alt="Map" width={1200} height={800} priority />
+        </div>
+      </main>
       <footer className={styles.footer}></footer>
     </div>
   );
